357a1cc3e56bed8e668ea2b7cca614d850702d1c
PW-Anh <nhatanhnd009@gmail.com>
2025-06-15 21:52:38 +0700

    updated ex.4

--- tests/lesson-2/ex4.js
+++ tests/lesson-2/ex4.js
@@ -1,7 +1,14 @@
-let myHeight = 157;
+let myHeight = 191;
 let myHeight1 = myHeight - 100 //số lẻ của chiều cao
-//console.log(myHeight1);
 let ideaWeight = myHeight1*9/10;
 let maxWeight = myHeight1;
 let minWeight = myHeight1*8/10;
-console.log("Ideal Weight:" , ideaWeight ,  "Min Weight:" , minWeight ,  "Max Weight:"+ maxWeight);
+if(myHeight>190 || myHeight<120)
+    {
+    console.log("the value is not valid")
+    }   
+if (120<=myHeight && myHeight<=190)
+    {
+    console.log("Ideal Weight:" , ideaWeight ,  "Min Weight:" , minWeight ,  "Max Weight:"+ maxWeight);
+    }  
+//updated 1 chut code
