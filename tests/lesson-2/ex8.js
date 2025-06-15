84d2e00824707ab2a958c192942b4d6d94e6dd00
PW-Anh <nhatanhnd009@gmail.com>
2025-06-15 15:37:42 +0700

    updated file ex.8

--- tests/lesson-2/ex8.js
+++ tests/lesson-2/ex8.js
@@ -3,3 +3,15 @@ for(i; i<=100; i=i+4){
     console.log(i)
         
 }
+// Nếu dùng if thì đc ko? c viết nhưng ko work đúng
+//let i=1;
+//for(i; i<=100; i++){
+ //   if(i==1)
+ //       {
+//            console.log(i)
+// }
+ //   if(i=i+4)
+  //      {
+ //           console.log(i)
+ //   }
+        
